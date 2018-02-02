1be46ba735e1d3e622b2a3ea6995846afda88f3f
binwang <binwang.qd@gmail.com>
2018-02-02 18:04:27 -0500

    kia tes

---
+++ src/geometry/example/KIA.kt
@@ -0,0 +1,14 @@
+package geometry.example
+
+
+import  geometry.shapes.*
+
+fun main(args: Array<String>) {
+//    val rec = Rectangle(41, 41)
+//    println(rec.isSquare)
+//    println(rec.notSQUARE())
+
+    print(createRandomRectangle().isSquare)
+}
+
+
